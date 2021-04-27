5419be6173c75b053282c29dc21ef09ea7b7b070
Ezequielotero <eze.oteroo@gmail.com>
2021-04-27 13:18:11 -0300

    push

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 // Syncing all the models at once.
-  server.listen(3001, () => {
+  server.listen(3001,'localhost', () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 
